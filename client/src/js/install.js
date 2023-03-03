6f6b1d225dccc05c51981214ad5f5e38a2577127
KateRitchie <ritchie_ecen@hotmail.com>
2023-03-02 17:08:50 -0700

    event

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,6 +9,20 @@ window.addEventListener('beforeinstallprompt', (event) => {
     console.log('beforeinstall', event)
 });
 
+/*
+alt code
+ const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+   return;
+  }
+
+  promptEvent.prompt();
+  
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);*/
+
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
   const promptEvent = window.deferredPrompt;
