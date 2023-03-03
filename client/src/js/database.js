75244f637f25137fca21318618f51419c2e9933f
KateRitchie <ritchie_ecen@hotmail.com>
2023-03-02 17:06:14 -0700

    event handlers

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (id, content) => {
-  console.error('Update database');
+  //console.error('Update database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -6,11 +6,22 @@ window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
     deferredPrompt = event;
     butInstall.hidden = false;
+    console.log('beforeinstall', event)
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-   
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+   return;
+  }
+
+  promptEvent.prompt();
+  
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
