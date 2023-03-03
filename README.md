45722e5ae0b0cf9fbbe4a0357e0dc51941c6918b
KateRitchie <ritchie_ecen@hotmail.com>
2023-03-03 07:00:31 -0700

    install button

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-# textEditor
-JATE
+# Text Editor
+JATE - Just Another Text Editor
+
+## Description
+
+This application allows users to create notes or text snippets in a text editor that can be used on or offline.  Users can reliably retrieve data for later use.
+
+## Installation
+
+git clone the repo to your local machine. To use this application, run the following command to install the dependencies:
+
+* npm install
+
+Then run the following command:
+
+* npm run start:dev
+
+## Usage
+
+Deployed site:[J.A.T.E]
 
 ## Credits
 
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,31 +2,27 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
-    deferredPrompt = event;
-    butInstall.hidden = false;
+/*window.addEventListener('beforeinstallprompt', (event) => {
+  console.log("INSTALLING!", event)
+    //event.preventDefault();
+    window.deferredPrompt = event;
+    console.log("WINDOW", window.deferredPrompt)
+    butInstall.classList.toggle('hidden', false)
     console.log('beforeinstall', event)
+});*/
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  console.log("INSTALLING!", event)
+  window.deferredPrompt = event;
+  console.log("WINDOW", window.deferredPrompt)
+  butInstall.classList.toggle('hidden', false);
 });
-
-/*
-alt code
- const promptEvent = window.deferredPrompt;
-
-  if (!promptEvent) {
-   return;
-  }
-
-  promptEvent.prompt();
-  
-  window.deferredPrompt = null;
-  
-  butInstall.classList.toggle('hidden', true);*/
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+  console.log('hello2')
   const promptEvent = window.deferredPrompt;
-
+  console.log("PROMPTENVEN>>>>",promptEvent)
   if (!promptEvent) {
    return;
   }
@@ -36,10 +32,11 @@ butInstall.addEventListener('click', async () => {
   window.deferredPrompt = null;
   
   butInstall.classList.toggle('hidden', true);
+  console.log('goodbye')
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    deferredPrompt = null;
+    window.deferredPrompt = null;
   console.log('appinstalled', event);
 });
